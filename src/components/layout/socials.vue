eaee3ae1f34e508289e1bf69368e52870aec5b35
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-02-06 16:21:45 +0100

    feat: created and positioned socials component

---
+++ src/components/layout/socials.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul id="socials">
+    <li>a</li>
+    <li>a</li>
+    <li>a</li>
+    <li>a</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+#socials {
+  position: fixed;
+  top: 60px;
+  right: 40px;
+}
+</style>
