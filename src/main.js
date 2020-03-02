c61d874ae96f2ba5e49c182a22564c3e8566aea4
Damilola Oluwafemi <damilolaoluwafemi@Damilolas-MacBook-Pro.local>
2020-03-02 16:42:11 +0100

    update: added aos and p5js

--- src/main.js
+++ src/main.js
@@ -2,23 +2,25 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import gsap from 'gsap';
 
 import ProfileLayout from './components/layout/ProfileLayout';
 import SocialItems from './components/layout/SocialItems';
 import Navbar from './components/layout/navbar'
 import ScrollIndicator from './components/layout/ScrollIndicator';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 Vue.component("profile-layout", ProfileLayout);
 Vue.component("social-items", SocialItems);
 Vue.component('scroll-indicator', ScrollIndicator);
 Vue.component('navbar', Navbar);
 
-Vue.use(gsap)
-
 Vue.config.productionTip = false;
 
 new Vue({
+  created() {
+    AOS.init({ disable: "phone" });
+  },
   router,
   store,
   render: h => h(App)
